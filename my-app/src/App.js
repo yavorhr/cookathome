87ae3a985e8919eaa4de748e031da9b871ced9c9
wolfie <yavorhristozov@gmail.com>
2022-11-15 12:04:56 +0200

    Login Component commit
    Create Login Component with <Navigation/> child

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import HomePage from "./Components/Home/HomePage.js";
+import Login from "./Components/Login/Login.js";
 
 function App() {
     return (
         <div className="App">
 
-            <HomePage />
+            {/* <HomePage /> */}
 
+            <Login />
         </div>
 
     );
---
+++ my-app/src/Components/Login/Login.js
@@ -0,0 +1,30 @@
+import './login.css';
+import Navigation from '../Navigation/Navigation.js';
+
+export default function Login() {
+    return (
+        <main>
+            <Navigation />
+            <section className="login-section mrgn-auto">
+                <h1>Login</h1>
+                <form action="">
+                    <div className="text-field">
+                        <input type="text" required />
+                        <span></span>
+                        <label forname="username">Username</label>
+                    </div>
+                    <div className="text-field">
+                        <input type="password" required />
+                        <span></span>
+                        <label forname="password">Password</label>
+                    </div>
+                    <button type="submit">Login</button>
+                    <div className="sign-up-link">
+                        <p>Not a member ?</p>
+                        <a href="#">Sign up</a>
+                    </div>
+                </form>
+            </section>
+        </main>
+    );
+}
