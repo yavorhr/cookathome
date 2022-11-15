ba86bfcbef651c8e51393965d7dbcb52accc7256
wolfie <yavorhristozov@gmail.com>
2022-11-15 13:47:56 +0200

    Cataloge Articles component initial commit
    Add Catalog Articles component with CSS

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,17 +3,21 @@ import HomePage from "./Components/Home/HomePage.js";
 import Login from "./Components/Login/Login.js";
 import Register from "./Components/Register/Register.js";
 import Profile from "./Components/Profile/Profile.js";
+import CatalogArticles from "./Components/CatalogeArticles/CatalogeArticles.js";
 
 function App() {
     return (
         <div className="App">
             <Navigation />
-        
+
             {/* <Login /> */}
 
             {/* <Profile /> */}
 
             {/* <Register/> */}
+
+            <CatalogArticles />
+
         </div>
 
     );
---
+++ my-app/src/Components/CatalogeArticles/CatalogeArticles.js
@@ -0,0 +1,94 @@
+import './catalog.css';
+
+export default function CatalogArticles() {
+    return (
+        <div>
+            <section className="articles">
+                <article className="section-info">
+                    <h1 className="title">Breakfast Ideas</h1>
+                    <p className="descr">Breakfast is often called 'the most important meal of the day', and for good reason
+                    </p>
+                    <p className="descr">It also gives you the energy you need to get things done and helps you focus at work or
+                        at school. Those are just a few reasons why its the most important meal of the day.</p>
+                </article>
+                <ul type="none" className="image-grid">
+                    <li className="article-item">
+                        <a href="">
+                            <img src="/img/dishes/eggsausage.jpg" alt="recipe details" />
+                            <div className="info">
+                                <h3>Egg & Sausage</h3>
+                                <p>08 August <span>Admin</span></p>
+                            </div>
+                        </a>
+                    </li>
+                    <li className="article-item">
+                        <a href="">
+                            <img src="/img/dishes/muesli.jpg" alt="recipe details" />
+                            <div className="info">
+                                <h3>Muesli</h3>
+                                <p>08 August <span>Admin</span></p>
+                            </div>
+                        </a>
+                    </li>
+                    <li className="article-item">
+                        <a href="">
+                            <img src="/img/dishes/pancakes.jpg" alt="recipe details" />
+                            <div className="info">
+                                <h3>Home made pancakes</h3>
+                                <p>08 August <span>Admin</span></p>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </section>
+            <section className="rest-articles-section">
+                <ul className="rest-articles" type="none">
+                    <li>
+                        <a href="">
+                            <div className="card">
+                                <img src="/img/dishes//bananas.jpg" alt="" />
+                                <div className="card-info">
+                                    <h3>Bananas are rich on calcium </h3>
+                                    <p className="date">10 June <span>Admin</span></p>
+                                    <p className="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, eligendi sunt
+                                        ullam aperiam quis quisquam et cum, assumenda veritatis quasi numquam odio. Vitae
+                                        voluptatem labore distinctio sint quaerat quasi sed.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <div className="card">
+                                <img src="/img/dishes//bananas.jpg" alt="" />
+                                <div className="card-info">
+                                    <h3>Bananas are rich on calcium </h3>
+                                    <p className="date">10 June <span>Admin</span></p>
+                                    <p className="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, eligendi sunt
+                                        ullam aperiam quis quisquam et cum, assumenda veritatis quasi numquam odio. Vitae
+                                        voluptatem labore distinctio sint quaerat quasi sed.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <div className="card">
+                                <img src="/img/dishes//bananas.jpg" alt="" />
+                                <div className="card-info">
+                                    <h3>Bananas are rich on calcium </h3>
+                                    <p className="date">10 June <span>Admin</span></p>
+                                    <p className="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, eligendi sunt
+                                        ullam aperiam quis quisquam et cum, assumenda veritatis quasi numquam odio. Vitae
+                                        voluptatem labore distinctio sint quaerat quasi sed.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+
+            </section>
+
+        </div >
+    );
+} 
