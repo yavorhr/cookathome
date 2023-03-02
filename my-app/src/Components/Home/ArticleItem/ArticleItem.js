d6ce9eb295acd3dd34f35d8ecd45e06003174bb6
wolfie <yavorhristozov@gmail.com>
2023-03-02 11:48:42 +0200

    Refactor : Create ArticleItem component in HomePage
    Create ArticleItem component styled with Module CSS,.
    This way it can be re-used when rendering the articles gallery in HomePage

--- my-app/src/Components/Home/ArticleItem/ArticleItem.js
+++ my-app/src/Components/Home/ArticleItem/ArticleItem.js
@@ -1,1 +1,13 @@
-export 
+import styles from './ArticleItem.module.css';
+
+export const ArticleItem = () => {
+    <li className={styles["article-item"]}>
+        <a href="">
+            <img src="/img/dishes/eggsausage.jpg" alt="recipe details" />
+            <div className={styles["article-item__info"]}>
+                <h3>Egg & Sausage</h3>
+                <p>08 August <span>Admin</span></p>
+            </div>
+        </a>
+    </li>
+}
