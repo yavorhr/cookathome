ade4273a9a19afaf80d345aaad37b862db294406
wolfie <yavorhristozov@gmail.com>
2023-03-07 14:26:04 +0200

    Add : DropdownItem CSS module stylization

--- my-app/src/Components/common/Navigation/DropdownMenu/DropdownItem/DropdownItem.js
+++ my-app/src/Components/common/Navigation/DropdownMenu/DropdownItem/DropdownItem.js
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
+import styles from './DropdownItem.module.css';
 
 export const DropdownItem = (props) => {
     return (
-        <Link to={props.url} className="menu-item" onClick={() => props.goToMenu && props.setActiveMenu(props.goToMenu) }>
-            <span className="icon-button">{props.leftIcon}</span>
+        <Link to={props.url} className={styles["menu-item"]} onClick={() => props.goToMenu && props.setActiveMenu(props.goToMenu)}>
+            <span className={styles["icon-button"]}>{props.leftIcon}</span>
 
             {props.children}
 
-            <span className="icon-right">{props.rightIcon}</span>
+            <span className={styles["icon-right"]}>{props.rightIcon}</span>
         </Link>
     );
 }
