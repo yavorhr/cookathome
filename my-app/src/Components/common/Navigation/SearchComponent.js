29e5f4339ed58fb9833c8524f38384247b87df20
wolfie <yavorhristozov@gmail.com>
2023-03-07 14:12:45 +0200

    Add : Search comp. CSS module stylization

--- my-app/src/Components/common/Navigation/SearchComponent.js
+++ my-app/src/Components/common/Navigation/SearchComponent.js
@@ -1,28 +1,27 @@
 
 
+import styles from './Search.module.css';
+
 import { useState } from "react";
 import { ReactComponent as Search } from './svg/search.svg';
 
 export const SearchComponent = () => {
     const [clicked, setClicked] = useState(false);
-   
-     const setClickHandler = () =>{
+
+    const setClickHandler = () => {
         setClicked(!clicked)
     }
 
     return (
-        <div className="search-comp">
-            {clicked &&  <form action="">
+        <div className={styles["search-comp"]}>
+            {clicked && <form action="">
                 <input type="text" />
             </form>}
 
-            <div className='nav-item-wrapper'>
-                <li className="nav-item">
-                    <a href="#" className="icon-button" onClick={() => setClickHandler()} >
-                        <Search />
-                    </a>
-                </li>
-
-            </div>
+            <li className={styles["nav-item"]}>
+                <a href="#" className={styles["icon-button"]} onClick={() => setClickHandler()} >
+                    <Search />
+                </a>
+            </li>
         </div>);
 }
