1425eb9c304ebd79df1520cf1a31c153a17d9d3b
wolfie <yavorhristozov@gmail.com>
2023-03-07 13:51:13 +0200

    Add : NavItemDropdown comp.

---
+++ my-app/src/Components/common/Navigation/NavItemDropdown/NavItemDropdown.js
@@ -0,0 +1,26 @@
+
+
+import { useState } from "react";
+
+import { DropdownMenu } from "./DropdownMenu.js";
+
+export const NavItemDropdown = (props) => {
+    const [open, setOpen] = useState(false);
+
+    const recipesData = [{ menu: 'season', url: ['recipes/summer', 'recipes/winter', 'recipes/autumn', 'recipes/spring'] }]
+    const articlesData = [{ menu: 'rec', url: ['1', '2', '3', '4'] }]
+
+    return (
+        <div className='nav-item-dropdown-wrapper'>
+            <li className="nav-item">
+                <a href="#" className="icon-button" onClick={() => setOpen(!open)} >
+                    {props.icon}
+                </a>
+                {open && props.title === 'Recipes' && <DropdownMenu data={recipesData} />}
+                {open && props.title === 'Articles' && <DropdownMenu data={articlesData} />}
+            </li>
+
+            {props.title && <span>{props.title}</span>}
+
+        </div>);
+}
