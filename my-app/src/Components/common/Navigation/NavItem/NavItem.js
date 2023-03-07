0c0f063cc9699f4dbc8e7f8c2b8a80adaa5cbbc7
wolfie <yavorhristozov@gmail.com>
2023-03-07 13:49:28 +0200

    Add : NavItem CSS module stylization

--- my-app/src/Components/common/Navigation/NavItem/NavItem.js
+++ my-app/src/Components/common/Navigation/NavItem/NavItem.js
@@ -1,11 +1,12 @@
 
+import styles from './NavItem.module.css';
 import { Link } from "react-router-dom";
 
 export const NavItem = (props) => {
 
     return (
-        <li className="nav-item">
-            <Link href="#" className="icon-button"  >
+        <li className={styles["nav-item"]}>
+            <Link to="#" className={styles["icon-button"]}  >
                 {props.icon}
             </Link>
         </li>
