9888936d8d54cb6ae4450c1f9937ed90b3505832
wolfie <yavorhristozov@gmail.com>
2023-03-22 19:35:37 +0200

    refact : Recipes added by user in Profile page
    Remove description from recipe card
    Add recipe name

--- client/src/Components/Profile/CardItem/CardItem.js
+++ client/src/Components/Profile/CardItem/CardItem.js
@@ -6,14 +6,13 @@ import { faClock } from '@fortawesome/free-solid-svg-icons';
 
 export const CardItem = ({
     recipe }) => {
-
+        console.log(recipe);
     return (
         <li>
             <Link to={`/details/${recipe._id}`}>
                 <div className={styles["card-item"]}>
                     <img className={styles["card-item__img"]} src={recipe.imageUrl} alt="" />
-                    <h3 className={styles["card-title"]}>{recipe.title}</h3>
-                    <p className={styles["card-descr"]}>{recipe.descr}</p>
+                    <p className={styles["card-title"]}>{recipe.name}</p>
                     <span>
                         <FontAwesomeIcon icon={faClock} className={styles["icon"]}></FontAwesomeIcon>{recipe["cook-time"]} min
                     </span>
