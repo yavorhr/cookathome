4f5413fe8c69ee06f5bf7917f8b4282b3721b3a1
wolfie <yavorhristozov@gmail.com>
2023-04-04 17:50:47 +0300

    feat : Add search in Catalog Recipes
    User is able to search through catalog of filtered recipes - for example search for a recipe in section "Summer recipes"

--- client/src/Components/CatalogRecipes/CatalogRecipes.js
+++ client/src/Components/CatalogRecipes/CatalogRecipes.js
@@ -1,32 +1,62 @@
 import styles from './CatalogRecipes.module.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import * as recipeService from "../../service/recipeService.js";
 
-
 import { CardItem } from '../Profile/CardItem/CardItem.js';
-import { Search } from '../Search/Search.js';
 
 export const CatalogRecipes = ({ }) => {
     const [recipes, setRecipes] = useState([]);
+    const [filteredRecipes, setFilteredRecipes] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
+
+    console.log(recipes);
+
     const { category, type } = useParams();
 
     useEffect(() => {
         recipeService
             .getRecipesByCategoryAndType(category, type)
-            .then(result => setRecipes(result))
+            .then(result => {
+                setRecipes(result)
+                setFilteredRecipes(result)
+            })
     }, [category, type]);
+
+    const onSearchSubmit = (e) => {
+        e.preventDefault();
+
+        const { search } = Object.fromEntries(new FormData(e.target));
+
+        setFilteredRecipes(recipes.filter(r => r.name.toLowerCase().includes(search.toLowerCase())));
+        setSearchValue('');
+    }
 
     return (
         <section>
-            
+
             <article>
-                {recipes.length > 0 && 
+                {recipes.length > 0 &&
                     <>
                         <h2 className={styles["section-title"]}>{type} recipes</h2>
+
+                        <form className={styles["search-form"]} action="" onSubmit={onSearchSubmit}>
+                            <input
+                                type="text"
+                                name="search"
+                                placeholder="Search here..."
+                                id="search" />
+                              <button type="submit">
+                        <FontAwesomeIcon icon={faMagnifyingGlass} className={styles["icon"]}></FontAwesomeIcon>
+                    </button>
+                        </form>
+
                         <ul className={styles["card-list"]} type="none">
-                            {recipes.map(r => <CardItem recipe={r} key={r._id} />)}
+                            {filteredRecipes.map(r => <CardItem recipe={r} key={r._id} />)}
                         </ul>
                     </>
                 }
@@ -34,3 +64,4 @@ export const CatalogRecipes = ({ }) => {
         </section >
     );
 }
+
